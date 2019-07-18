24c584f38d74210d240196f43ad4f3f7a7b0f9e1
steven-horn <steven3556@outlook.com>
2019-07-18 00:25:14 -0400

    fixed commit mistake

--- app/src/main/java/com/malcolmscruggs/quandom/MainActivity.java
+++ app/src/main/java/com/malcolmscruggs/quandom/MainActivity.java
@@ -12,14 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.quickPlayButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, PlayerSelectionActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
