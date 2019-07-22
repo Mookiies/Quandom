6240a447687406f994e76c1d0613427cb47a32ca
scruggs <mookie.dev@gmail.com>
2019-07-22 19:22:08 -0400

    Exit app on home screen back press (don't allow going back to win screen)

--- app/src/main/java/com/malcolmscruggs/quandom/MainActivity.java
+++ app/src/main/java/com/malcolmscruggs/quandom/MainActivity.java
@@ -21,4 +21,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onBackPressed(){
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
 }
