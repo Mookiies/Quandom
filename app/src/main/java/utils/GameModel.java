261667d61b5a19866b4d9b0e63e43b47f4e86c4c
steven-horn <steven3556@outlook.com>
2019-07-30 17:22:42 -0400

    handle ties

--- app/src/main/java/utils/GameModel.java
+++ app/src/main/java/utils/GameModel.java
@@ -85,8 +85,25 @@ final public class GameModel implements Serializable {
         return this.isGameOver;
     }
 
-    public Player getWinningPlayer() {
-        return Collections.max(players);
+    public ArrayList<Player> getWinningPlayer() {
+        ArrayList<Integer> scores = new ArrayList<>();
+        for (Player player : players) {
+            scores.add(player.getPlayerScore());
+        }
+
+        ArrayList<Player> winners = new ArrayList<>();
+
+        int maxScore = Collections.max(scores);
+        int maxIndex = scores.indexOf(maxScore);
+        scores.set(maxIndex, -1);
+        winners.add(players.get(maxIndex));
+        for (int score : scores) {
+            if (score == maxScore) {
+                winners.add(players.get(scores.indexOf(score)));
+                scores.set(scores.indexOf(score), -1);
+            }
+        }
+        return winners;
     }
 
     public boolean guessQuestion(int guess) {
