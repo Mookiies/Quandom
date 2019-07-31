a2fa06faf9464daa3ed185906004cc67c87436b0
Anubhav Sharma <sharma.an@husky.neu.edu>
2019-07-31 01:25:14 -0400

    Added music to main activity

--- app/src/main/java/com/malcolmscruggs/quandom/MainActivity.java
+++ app/src/main/java/com/malcolmscruggs/quandom/MainActivity.java
@@ -1,19 +1,32 @@
 package com.malcolmscruggs.quandom;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean music;
+    private MediaPlayer musicMedia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set default value for music
+        //Source: http://freemusicarchive.org/music/Daniel_Birch/MUSIC_FOR_TV_FILM__GAMES_VOL1/The_Elevator_Game
+        musicMedia = MediaPlayer.create(MainActivity.this, R.raw.music);
+        music = false;
+        Switch musicSwitch = findViewById(R.id.musicSwitch);
+        musicSwitch.setChecked(music);
 
         Button quickPlay = findViewById(R.id.quickPlayButton);
         quickPlay.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +43,19 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent customPlayIntent = new Intent(getApplicationContext(), CustomPlayActivity.class);
                 startActivity(customPlayIntent);
+            }
+        });
+
+        musicSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                music = b;
+                compoundButton.setChecked(music);
+                if (music) {
+                    musicMedia.start();
+                } else {
+                    musicMedia.pause();
+                }
             }
         });
     }
