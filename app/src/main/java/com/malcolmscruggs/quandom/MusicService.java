de33c63c302899823eba01e5f1c6de7e0de0d6be
scruggs <mookie.dev@gmail.com>
2019-08-01 11:43:48 -0400

    release media obj in MusicService

--- app/src/main/java/com/malcolmscruggs/quandom/MusicService.java
+++ app/src/main/java/com/malcolmscruggs/quandom/MusicService.java
@@ -4,7 +4,6 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
-import android.widget.Toast;
 
 public class MusicService extends Service {
     MediaPlayer mediaPlayer;
@@ -16,21 +15,18 @@ public class MusicService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "Music started.", Toast.LENGTH_SHORT).show();
         mediaPlayer.start();
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onCreate() {
-        Toast.makeText(this, "Music service created.", Toast.LENGTH_SHORT).show();
         mediaPlayer = MediaPlayer.create(this, R.raw.music);
         mediaPlayer.setLooping(true);
     }
 
     @Override
     public void onDestroy() {
-        Toast.makeText(this, "Music service destroyed.", Toast.LENGTH_SHORT).show();
         mediaPlayer.stop();
         mediaPlayer.release();
     }
